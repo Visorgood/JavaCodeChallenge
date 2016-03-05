315939064367f99eefc9880e549104108effdd13
Viacheslav <viacheslav.inozemtsev@gmail.com>
2016-03-05 20:20:44 +0100

    some changes

--- Transactions/src/main/java/controller/TransactionServiceController.java
+++ Transactions/src/main/java/controller/TransactionServiceController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,9 +19,11 @@ public class TransactionServiceController {
     private final ConcurrentMap<Long, Transaction> transactions = new ConcurrentHashMap<>();
 
     @RequestMapping(value = "/transaction/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
     public void putTransaction(
             @PathVariable("id") Long id,
             @RequestBody Transaction transaction) {
+        System.out.println("Adding transaction with id: " + id);
         transactions.put(id, transaction);
     }
 
@@ -27,6 +31,7 @@ public class TransactionServiceController {
     @ResponseBody
     public Transaction getTransaction(
             @PathVariable("id") Long id) {
+        System.out.println("Returning transaction with id: " + id);
         return transactions.get(id);
     }
 }
